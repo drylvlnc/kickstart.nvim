cb781714a3baec39a521c5f679dab74bd8dada4c
excadryl <drylvalencia@gmail.com>
2025-09-03 01:15:46 +0800

    Add indentation lines with ibl.nvim

--- init.lua
+++ init.lua
@@ -249,6 +249,14 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
+  { -- Indentation guides
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {
+      indent = { char = '│' },
+      scope = { enabled = true },
+    },
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
